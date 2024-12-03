942a46cda1636bdebf8cccfb435b0bf9e9c63065
Julian Meyer <julian.meyer@coinbase.com>
2024-12-03 09:56:37 -0800

    chore(rpc): update execution witness to match spec

--- crates/rpc-types-debug/src/debug.rs
+++ crates/rpc-types-debug/src/debug.rs
@@ -1,25 +1,18 @@
 //! Types for the `debug` API.
 
-use alloy_primitives::{map::B256HashMap, Bytes};
+use alloy_primitives::Bytes;
 use serde::{Deserialize, Serialize};
 
 /// Represents the execution witness of a block. Contains an optional map of state preimages.
 #[derive(Clone, Debug, Default, PartialEq, Eq, Serialize, Deserialize)]
 pub struct ExecutionWitness {
-    /// Map of all hashed trie nodes to their preimages that were required during the execution of
+    /// Trie nodes that were required during the execution of
     /// the block, including during state root recomputation.
-    ///
-    /// `keccak(rlp(node)) => rlp(node)`
-    pub state: B256HashMap<Bytes>,
-    /// Map of all contract codes (created / accessed) to their preimages that were required during
+    pub state: Vec<Bytes>,
+    /// Contract bytecode preimages that were required during
     /// the execution of the block, including during state root recomputation.
-    ///
-    /// `keccak(bytecodes) => bytecodes`
-    pub codes: B256HashMap<Bytes>,
-    /// Map of all hashed account and storage keys (addresses and slots) to their preimages
-    /// (unhashed account addresses and storage slots, respectively) that were required during
-    /// the execution of the block. during the execution of the block.
-    ///
-    /// `keccak(address|slot) => address|slot`
-    pub keys: B256HashMap<Bytes>,
+    pub codes: Vec<Bytes>,
+    /// Account and storage trie node preimages that were required during
+    /// the execution of the block.
+    pub keys: Vec<Bytes>,
 }
